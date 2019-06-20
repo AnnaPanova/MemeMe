9bfed706bb7696cd09131ff63823a500df49d1d0
AnnaPanova <mia-anchik@yandex.ru>
2019-06-20 16:32:06 +0200

    no message

---
+++ MemeMe/CustomTextFieldDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  CustomTextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Анна on 20/06/2019.
+//  Copyright © 2019 Anna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class CustomTextFieldDelegate: NSObject, UITextFieldDelegate {
+
+    var topEditedText: String?
+    var bottomEditedText: String?
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" {
+            textField.text = ""
+        } else if textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.tag == 0 {
+            topEditedText = textField.text
+        } else {
+            bottomEditedText = textField.text
+        }
+        return true
+    }
+
+}
---
+++ MemeMe/Meme.swift
@@ -0,0 +1,20 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Анна on 20/06/2019.
+//  Copyright © 2019 Anna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+struct Meme {
+    let topText: String
+    let bottomText : String
+    let originalImage: UIImage
+    let memedImage : UIImage
+    
+}
